8015cbecb756c52a8b7a9fb455e7df3ad2ee19ee
Kraego <tkraxner@gmx.at>
2020-10-17 13:03:37 +0200

    added tranformation debug

--- Occupancy_Plotter.py
+++ Occupancy_Plotter.py
@@ -3,6 +3,9 @@ import math
 import numpy as np
 
 
+DEBUG_TRANSFORMATION = False
+
+
 def __calculate_robot_coordinate(alpha, distance):
     alpha_rads = math.radians(alpha)
     x = math.sin(alpha_rads) * distance
@@ -26,11 +29,22 @@ def transform_to_world(robot_coordinates, x_offset, y_offset, theta):
         x = robot_coordinates[0][point]
         y = robot_coordinates[1][point]
         robot_point = np.array([[x], [y], [1]])
-        result = transformation_matrix.dot(robot_point)
+        result = np.matmul(transformation_matrix, robot_point)
+        if DEBUG_TRANSFORMATION:
+            print_transformation(result, robot_point, transformation_matrix)
         transformed_x.append(result[0])
         transformed_y.append(result[1])
 
     return transformed_x, transformed_y
+
+
+def print_transformation(result, robot_point, transformation_matrix):
+    print('\n#########################################' + '\n')
+    print(robot_point)
+    print('\n\tx\n')
+    print(transformation_matrix)
+    print('\n\t=\n')
+    print(result)
 
 
 def calculate_robot_coordinates(robot_data_points):
